2ee3ce0b1ad6b53caf2dc9d8f2aa362b2526101f
kaparoo <kaparoo2001@gmail.com>
2022-01-25 14:20:40 +0900

    :sparkles: Add activation to Decoder and Encoder

--- conv_tasnet/layers/autoencoder.py
+++ conv_tasnet/layers/autoencoder.py
@@ -2,17 +2,18 @@
 
 import tensorflow as tf
 import tensorflow.keras as keras
-from typing import Dict
+from typing import Any, Dict
 
 
 __all__ = ["Decoder", "Encoder"]
 
 
 class Encoder(keras.layers.Layer):
-    def __init__(self, num_filters: int, filter_len: int):
+    def __init__(self, num_filters: int, filter_len: int, activation=None):
         super(Encoder, self).__init__()
         self.num_filters = num_filters
         self.filter_len = filter_len
+        self.activation = activation
 
     def build(self, input_shape: tf.TensorShape):
         self.reshape = keras.layers.Reshape(
@@ -22,6 +23,7 @@ class Encoder(keras.layers.Layer):
             filters=self.num_filters,
             kernel_size=self.filter_len,
             strides=self.filter_len // 2,
+            activation=self.activation,
         )
 
     def call(self, input_mixture: tf.Tensor) -> tf.Tensor:
@@ -29,21 +31,27 @@ class Encoder(keras.layers.Layer):
         mixture_weights = self.encode(reshaped_mixture)
         return mixture_weights
 
-    def get_config(self) -> Dict[str, int]:
-        return {"num_filters": self.num_filters, "filter_len": self.filter_len}
+    def get_config(self) -> Dict[str, Any]:
+        return {
+            "num_filters": self.num_filters,
+            "filter_len": self.filter_len,
+            "activation": self.activation,
+        }
 
 
 class Decoder(keras.layers.Layer):
-    def __init__(self, num_filters: int, filter_len: int):
+    def __init__(self, num_filters: int, filter_len: int, activation=None):
         super(Decoder, self).__init__()
         self.num_filters = num_filters
         self.filter_len = filter_len
+        self.activation = activation
 
     def build(self, input_shape: tf.TensorShape):
         self.decode = keras.layers.Conv2DTranspose(
             filters=self.num_filters,
             kernel_size=(1, self.filter_len),
             strides=(1, self.filter_len // 2),
+            activation=self.activation,
             input_shape=input_shape[1:],
         )
 
@@ -53,5 +61,9 @@ class Decoder(keras.layers.Layer):
             estimated_sources = tf.squeeze(estimated_sources, axis=-1)
         return estimated_sources
 
-    def get_config(self) -> Dict[str, int]:
-        return {"num_filters": self.num_filters, "filter_len": self.filter_len}
+    def get_config(self) -> Dict[str, Any]:
+        return {
+            "num_filters": self.num_filters,
+            "filter_len": self.filter_len,
+            "activation": self.activation,
+        }
