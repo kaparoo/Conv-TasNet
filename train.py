6f3be3912d65aba4bcd1620eea1490c867828a12
kaparoo <kaparoo2001@gmail.com>
2022-01-25 17:37:53 +0900

    :construction: Add train.py and utils.py for train pipeline

---
+++ train.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from absl import app
+from absl import flags
+
+import pathlib
+import utils
+
+
+FLAGS = flags.FLAGS
+
+
+def train(_):
+    try:
+        checkpoint_dir = pathlib.Path(FLAGS.checkpoint_dir)
+        model, initial_epoch = utils.prepare_model(checkpoint_dir)
+        train_dataset, valid_dataset = utils.prepare_dataset(checkpoint_dir)
+        callback_list = utils.prepare_callbacks(checkpoint_dir)
+        model.fit(
+            train_dataset,
+            validation_data=valid_dataset,
+            initial_epoch=initial_epoch,
+            epochs=FLAGS.num_epochs + initial_epoch,
+            callbacks=callback_list,
+        )
+    except KeyboardInterrupt:
+        model.save(checkpoint_dir / "model")
+
+
+if __name__ == "__main__":
+    utils.define_flags()
+    app.run(train)
---
+++ utils.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+
+from absl import flags
+from beartype import beartype
+
+import conv_tasnet
+import musdb18
+
+from pathlib import Path
+
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.keras.callbacks as callbacks
+
+from typing import List, Literal, Optional, Tuple, Union
+
+
+__all__ = [
+    "define_flags",
+    "load_model",
+    "prepare_callbacks",
+    "prepare_dataset",
+    "prepare_model",
+]
+
+Model = conv_tasnet
+ModelParam = conv_tasnet.ConvTasNetParam
+ModelParamArgs = lambda: {
+    "T": _FLAGS.input_length,
+    "C": _FLAGS.num_sources,
+    "N": _FLAGS.num_filters,
+    "L": _FLAGS.filter_length,
+    "B": _FLAGS.residual_channels,
+    "S": _FLAGS.skipconn_channels,
+    "H": _FLAGS.convblock_channels,
+    "P": _FLAGS.dconv_kernel,
+    "X": _FLAGS.num_blocks,
+    "R": _FLAGS.num_repeats,
+    "causal": _FLAGS.causal,
+}
+
+Dataset = musdb18
+DatasetParam = musdb18.MUSDB18Param
+DatasetParamArgs = lambda: {}
+
+_FLAGS = flags.FLAGS
+_CONFIG_FILENAME = "config.json"
+_CSVLOG_FILENAME = "log.csv"
+
+
+@beartype
+def define_flags(mode: Literal["train", "test"] = "train"):
+    flags.DEFINE_string("checkpoint_dir", None, "Directory of results", required=True)
+    if mode == "train":
+        flags.DEFINE_integer(
+            "num_epochs", 3, "Number of epochs", lower_bound=0, upper_bound=99999
+        )
+    Model.define_flags()
+    Dataset.define_flags()
+
+
+def _initialize_model_param(checkpoint_dir: Path) -> ModelParam:
+    model_param = Model.make_param(ModelParamArgs())
+    model_param.save(checkpoint_dir / _CONFIG_FILENAME, overwrite=True)
+    return model_param
+
+
+@beartype
+def prepare_model(checkpoint_dir: Path) -> Tuple[keras.Model, int]:
+    model, initial_epoch = None, 0
+
+    if not checkpoint_dir.exists():
+        checkpoint_dir.mkdir(parents=True, exist_ok=True)
+        model_param = _initialize_model_param(checkpoint_dir)
+    elif not checkpoint_dir.is_dir():
+        raise FileExistsError("not a directory: %s" % checkpoint_dir)
+    else:
+        try:
+            model_param = Model.load_param(checkpoint_dir / _CONFIG_FILENAME)
+        except (FileNotFoundError, KeyError):
+            model_param = _initialize_model_param(checkpoint_dir)
+        except Exception as e:
+            raise e
+        finally:
+            checkpoints = sorted(checkpoint_dir.glob("*.ckpt*"), reverse=True)
+            for checkpoint in checkpoints:
+                if checkpoint.is_file():
+                    latest_checkpoint = checkpoint.with_suffix("")
+                    model = Model.make_model(model_param)
+                    model.load_weights(latest_checkpoint)
+                    initial_epoch = int(latest_checkpoint.stem)
+                    break
+
+    if not model:
+        model = Model.make_model(model_param)
+
+    return model, initial_epoch
+
+
+@beartype
+def load_model(
+    checkpoint_dir: Path, checkpoint_idx: Optional[int] = None
+) -> Tuple[keras.Model, ModelParam]:
+    if not checkpoint_dir.exists():
+        raise FileNotFoundError("no such directory: %s" % checkpoint_dir)
+    elif not checkpoint_dir.is_dir():
+        raise FileExistsError("not a directory: %s" % checkpoint_dir)
+    else:
+        model_param = Model.load_param(checkpoint_dir / _CONFIG_FILENAME)
+        if checkpoint_idx is not None and checkpoint_idx > 0:
+            checkpoint_path = checkpoint_dir / f"{checkpoint_idx:05d}.ckpt"
+            if not checkpoint_path.exists():
+                raise FileNotFoundError("no such file: %s" % checkpoint_path)
+        else:
+            checkpoints = sorted(checkpoint_dir.glob("*.ckpt*"), reverse=True)
+            for checkpoint in checkpoints:
+                if checkpoint.is_file():
+                    latest_checkpoint = checkpoint.with_suffix("")
+                    model = Model.make_model(model_param)
+                    model.load_weights(latest_checkpoint)
+                    return model, model_param
+            else:
+                raise FileNotFoundError("no *.ckpt file: %s" % checkpoint_dir)
+
+
+def _initialize_dataset_param(checkpoint_dir: Path) -> DatasetParam:
+    dataset_param = Dataset.make_param(DatasetParamArgs())
+    dataset_param.save(checkpoint_dir / _CONFIG_FILENAME, overwrite=True)
+    return dataset_param
+
+
+@beartype
+def prepare_dataset(
+    checkpoint_dir: Path, mode: Literal["train", "test"] = "train"
+) -> Tuple[tf.data.Dataset, Union[tf.data.Dataset, None]]:
+    if not checkpoint_dir.exists():
+        checkpoint_dir.mkdir(parents=True, exist_ok=True)
+        dataset_param = _initialize_dataset_param(checkpoint_dir)
+    elif not checkpoint_dir.is_dir():
+        raise FileExistsError("not a directory: %s" % checkpoint_dir)
+    else:
+        try:
+            dataset_param = Dataset.load_param(checkpoint_dir / _CONFIG_FILENAME)
+        except (FileNotFoundError, KeyError):
+            dataset_param = _initialize_dataset_param(checkpoint_dir)
+        except Exception as e:
+            raise e
+
+    dataset_root = Path(dataset_param.dataset_root)
+    if not dataset_root.exists():
+        raise FileNotFoundError("no such directory: %s" % dataset_root)
+    else:
+        ...
+
+
+@beartype
+def prepare_callbacks(
+    path: Path,
+    callback_list: List[callbacks.Callback] = [],
+    mode: Literal["train", "test"] = "train",
+) -> List[callbacks.Callback]:
+    callback_list = callback_list[::].append(callbacks.TerminateOnNaN())
+    if mode == "train":
+        callback_list.extend(
+            [
+                callbacks.CSVLogger(path / _CSVLOG_FILENAME, append=True),
+                keras.callbacks.ModelCheckpoint(
+                    filepath=path / "{epoch:05d}.ckpt",
+                    monitor="loss",
+                    verbose=1,
+                ),
+            ]
+        )
+    return callback_list
