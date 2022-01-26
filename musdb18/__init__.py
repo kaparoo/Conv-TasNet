7fbe0a9954a6721ae0891cbc5a3966a2f6e07919
kaparoo <kaparoo2001@gmail.com>
2022-01-26 17:26:07 +0900

    :coffin: Remove dead function: get_label_names

--- musdb18/__init__.py
+++ musdb18/__init__.py
@@ -2,11 +2,10 @@
 
 from musdb18.flags import define_flags
 from musdb18.param import load_param, make_param, MUSDB18Param
-from musdb18.dataset import get_label_names, make_dataset
+from musdb18.dataset import make_dataset
 
 __all__ = [
     "define_flags",
-    "get_label_names",
     "load_param",
     "make_dataset",
     "make_param",
