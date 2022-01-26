3705a3bcfb70de0d5632e001d8bbd112a5d3c902
kaparoo <kaparoo2001@gmail.com>
2022-01-26 18:21:19 +0900

    :adhesive_bandage: Add missing parameter: checkpoint_idx to utils.load_model()

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ FLAGS = flags.FLAGS
 
 def train(_):
     checkpoint_dir = pathlib.Path(FLAGS.checkpoint_dir)
-    model = utils.load_model(checkpoint_dir)
+    model = utils.load_model(checkpoint_dir, FLAGS.checkpoint_idx)
     dataset, _ = utils.prepare_dataset(checkpoint_dir, mode="test")
     model.evaluate(dataset)
 
